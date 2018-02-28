594afb41f484eda42930ce542ba7f0ad94b6af71
Filip Hallqvist <filiphallqvist@gmail.com>
2018-02-28 15:30:44 +0100

    Added plots

--- cev.m
+++ cev.m
@@ -1,5 +1,5 @@
 % Plot CEV prices and Black-Scholes prices for vanilla options.
-
+%figure;
 clc;
 clear;
 hold on;
@@ -7,15 +7,15 @@ hold on;
 plotStyleCall = {'--b','-b','-.b', 'vb', '~b'};
 plotStylePut  = {'--r','-r','-.r', 'vr', '~r'};
 
-T   = 1;
-X   = 60;
-n   = 2000;
-m   = 400;
-K   = 30;
+T   = 1/2;
+X   = 100;
+n   = 100;
+m   = 500;
+K   = 10;
 g   = @(x) x - K;
-r   = 0.01;
+r   = 0.02;
 deltas = {.75, 1, 1.1};
-sig = 0.25;
+sig = 0.5;
 
 disp('Running...');
 disp(' ');
@@ -76,25 +76,39 @@ disp(' ');
 % OTHER
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Plot strike price
-line([K K], [0 (K + 0.1)], 'Color', 'k', 'LineStyle', ':');
-line([(3/5 * K) (3/5 * K)], [0 (K + 0.1)], 'Color', 'k', 'LineStyle', ':');
-line([(7/5 * K) (7/5 * K)], [0 (K + 0.1)], 'Color', 'k', 'LineStyle', ':');
-
 % Plot Black-Scholes
 [bsc, bsp] = blsprice(space, K, r, T, sig);
 
 plot(x, bsc, '.k');
 plot(x, bsp, '.k');
 
+% Plot strike price
+line([K K], [0 (K + 0.1)], 'Color', 'k', 'LineStyle', ':');
+% line([(3/5 * K) (3/5 * K)], [0 (K + 0.1)], 'Color', 'k', 'LineStyle', ':');
+% line([(7/5 * K) (7/5 * K)], [0 (K + 0.1)], 'Color', 'k', 'LineStyle', ':');
+
 % Set axis
 axis([0 (2 * K) 0 K]);
-% axis auto
+% axis auto;
+
 hold off;
+
+title('Vanilla put and call option pricing with the CEV model for strike K = 10');
+legend('delta = 0.75', 'delta = 1.00', 'delta = 1.10', 'delta = 0.75', 'delta = 1.00', 'delta = 1.10', 'Black-Scholes');
+xlabel('Stock price');
+ylabel('Option price');
+
+
+paper_width = 6;
+paper_height = 6;
+set(gcf, 'Units', 'Inches', 'Position', [0, 0, paper_width, paper_height], 'PaperUnits', 'Inches', 'PaperSize', [paper_width, paper_height])
+saveas(gcf,'figures/cev.pdf')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % PUT-CALL PARITY
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+figure;
 
 del = 0.5;
 [c_u, c_time, c_space] = cn(T, X, n, m, g, r, del, sig);
@@ -103,7 +117,7 @@ del = 0.5;
 c_x = zeros(1, m + 1); p_x = zeros(1, m + 1);
 c_y = zeros(1, m + 1); p_y = zeros(1, m + 1);
 
-parity = 0;
+parity_errors = zeros(1, m + 1);
 
 for j = 1 : m + 1
     c_x(j) = c_space(j);
@@ -112,10 +126,27 @@ for j = 1 : m + 1
     c_y(j) = exp(-(r * T)) * c_u(n + 1, j);
     p_y(j) = exp(-(r * T)) * p_u(n + 1, j);
     
-    parity = parity + abs(c_y(j) - p_y(j) - c_space(j) + K * exp(-r * T));
+    error = abs(c_y(j) - p_y(j) - c_space(j) + K * exp(-r * T));
+    parity_errors(j) = error;
 end
 
-disp(['Avg. Parity error = ', num2str(parity / (m + 1))]);
+avg_parity_error = mean(parity_errors);
+
+plot(c_x, parity_errors);
+hold on;
+line([0 120], [avg_parity_error avg_parity_error], 'Color', 'k', 'LineStyle', ':');
+hold off;
+
+title('Error in the put-call parity for numerically calculated CEV model');
+legend('error', 'average error', 'Location', 'northwest');
+xlabel('Stock price');
+ylabel('Error');
+axis([0 110 0 1.1]);
+
+paper_width = 6;
+paper_height = 6;
+set(gcf, 'Units', 'Inches', 'Position', [0, 0, paper_width, paper_height], 'PaperUnits', 'Inches', 'PaperSize', [paper_width, paper_height])
+saveas(gcf,'figures/parity.pdf')
 
 disp(' ');
 disp('Done!')
--- impvol.m
+++ impvol.m
@@ -1,42 +1,83 @@
 % Plot Black-Scholes implied volatility, where price is given by the CEV-model.
-clc;
+% clc;
 clear;
+figure;
 hold on;
 
-K_max = 40;
-x = zeros(1, K_max); 
+plotStyle = {'--b','-b','-.b', 'vb', '~b'};
 
-y_cev_c = zeros(1, K_max);
-y_cev_p = zeros(1, K_max);
-y_bls = zeros(1, K_max);
 
-for i = 1 : K_max
-    % Calculate initial call option price with strike K
-    K   = i;
-    X   = 200;
-    n   = 1000;
-    m   = 600;
-    T   = 1;                        % Years until expiry
-    g   = @(x) max(0, x - K);       % Option function
-    r   = 0.01;                     % Interest rate
-    del = 1;                        % CEV Delta
-    sig = .25;                      % Volatility
+% deltas = {1.499, 1.5, 1.501}; % Helt rak, startar på omkring 1.39
+deltas = {0.999, 1, 1.001}; % Böjen uppåt, startar på omkring 0.494
+% deltas = {0.499, 0.5, 0.501}; % Böjen nedåt, startar på omkring 0.053
 
-    [u_c, time_c, space_c] = cn(T, X, n, m, g, r, del, sig);
+ddeltas = {
+    {1.499, 1.5, 1.501}, % Helt rak, startar på omkring 1.39
+    {0.999, 1, 1.001},   % Böjen uppåt, startar på omkring 0.494
+    {0.499, 0.5, 0.501} % Böjen nedåt, startar på omkring 0.053
+};
 
-    si = round(m / 10);
-    s0 = space_c(si);
-    
-    cev_price_c = exp(-r * T) * u_c(n + 1, si);
-    bls_price = blsprice(s0, K, r, T, sig);
+for h = 1 : length(deltas)
+    deltas = ddeltas{h};
+    subplot(1, length(ddeltas), h);
+    hold on;
 
-    x(i) = K;
+    for k = 1 : length(deltas)
+        K_max = 120;
+        K_min = 80;
 
-    y_cev_c(i) = blsimpv(s0, K, r, T, cev_price_c, 1, 0, [], true);
-    y_bls(i) = blsimpv(s0, K, r, T, bls_price);
+        x = ones(1, K_max - K_min); 
+
+        y_cev_c = ones(1, K_max - K_min);
+        y_cev_p = ones(1, K_max - K_min);
+        y_bls = ones(1, K_max - K_min);
+
+        for i = K_min : K_max
+            K   = i;
+            X   = 200;
+            n   = 100;
+            m   = 500;
+            T   = 1;                        % Years until expiry
+            g   = @(x) max(0, x - K);       % Option function
+            r   = 0.01;                     % Interest rate
+            del = deltas{k};                % CEV Delta
+            sig = .25;                      % Volatility
+
+            [u_c, time_c, space_c] = cn(T, X, n, m, g, r, del, sig);
+
+            si = round(m / 2);
+            s0 = space_c(si);
+
+            cev_price_c = exp(-r * T) * u_c(n + 1, si);
+            bls_price = blsprice(s0, K, r, T, sig);
+
+            x(i - K_min + 1) = K;
+
+            y_cev_c(i - K_min + 1) = blsimpv(s0, K, r, T, cev_price_c);
+            y_bls(i - K_min + 1)   = blsimpv(s0, K, r, T, bls_price);
+        end
+        disp(['Price: ', num2str(s0)]);
+
+        plot(x, y_cev_c);
+        %line([s0 s0], [0 .1], 'Color', 'k', 'LineStyle', ':');
+
+        xlabel('Strike price');
+        ylabel('Implied volatility');
+        
+        if h == 2
+            title('Black-Scholes implied volatility for vanilla call option with price given by the CEV model');
+        end
+
+        axis auto;
+    end
+    legend("delta = " + deltas{1}, "delta = " + deltas{2}, "delta = " + deltas{3});
 end
 
-plot(x, y_cev_c, 'b');
-plot(x, y_bls, 'k');
+paper_width = 24;
+paper_height = 6;
+set(gcf, 'Units', 'Inches', 'Position', [0, 0, paper_width, paper_height], 'PaperUnits', 'Inches', 'PaperSize', [paper_width, paper_height])
+saveas(gcf,'figures/implied-volatility.pdf')
 
-axis auto;
+hold off;
+
+%plot(x, y_bls, 'k');
